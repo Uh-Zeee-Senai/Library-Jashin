38c1295c34c440f3d2fc46f5588c97371eee860b
Uh-Zeee-Senai <jose.l.carvalho8@aluno.senai.br>
2026-08-28 08:43:45 -0300

    2

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
 window.SUPABASE_CONFIG = {
-    url: 'https://ewbhpdgwxwvfrauggbdr.supabase.co',
-    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV3YmhwZGd3eHd2ZnJhdWdnYmRyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc3NjA1ODksImV4cCI6MjEwMzMzNjU4OX0.H71mXhQJp946PhT4rHvj7rRkHn5lrhicVMGpYMvVoFM'
+    url: 'https://vnkljkjteekgcmhvubas.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZua2xqa2p0ZWVrZ2NtaHZ1YmFzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc4OTU4MTEsImV4cCI6MjEwMzQ3MTgxMX0.niuuESnONg3K_QJmajqge-NzlO72KBgOeccjLy7mbBI'
 }
