c26713edaeea2e251e0fdea5879be9f0d423587a
Uh-Zeee-Senai <jose.l.carvalho8@aluno.senai.br>
2026-08-28 09:04:41 -0300

    4

--- auth.js
+++ auth.js
@@ -9,8 +9,8 @@
 // ============================================================
 
 const supabaseClient = window.supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_ANON_KEY
+    window.SUPABASE_CONFIG.url,
+    window.SUPABASE_CONFIG.anonKey
 );
 
 
